d869ece1f02c45c1c3cbc489d4c6c6e1b3ada39d
Hunter Kassow <hunterkassow@protonmail.com>
2022-06-06 17:41:09 -0400

    commit tree ed6cd16b3393f725470c43573aabf3a5fc6cfb86

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  
+  const x = links.map(link => <Boop bop={link} key={link}/>)
+  console.log(x)
+  return <nav>{x}</nav>;
 }
 
+function Boop({bop}){
+  return(
+  <a href={'#'+ bop}>{bop}</a>
+  )
+}
+console.log(Boop('hi'))
 export default NavBar;
